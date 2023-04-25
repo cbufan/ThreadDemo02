a495b37c5f2ac64490c5bb0a2ed62475f9bd78ec
cbufan <77486614+cbufan@users.noreply.github.com>
2023-04-25 23:28:40 +0800

    Update NumberController.java

--- src/main/java/com/chen/controller/NumberController.java
+++ src/main/java/com/chen/controller/NumberController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NumberController {
 
     //123123
-
+    //456
     @Autowired
     private NumberService numberService;
 
